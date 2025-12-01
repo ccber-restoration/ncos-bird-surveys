8f86cb1cfcff2c1a06d9cc0173b7cdf5a8aaf870
Matt Vinh <mattvinh@gmail.com>
2025-12-01 15:07:07 -0800

    2025-12-01 meeting notes

---
+++ code/from_Matt_Vinh/MV_2025-12-01.R
@@ -0,0 +1,24 @@
+breeding period - april to june or july
+ - still migrating in april
+ - late breeding in july
+
+density of watefowl birds or others
+- google scholar density of birds?
+  
+species richness per guild over time
+
+reading papers on stats methods, ecological knowledge relating to work being done
+
+indicator species analysis by year
+
+GUILDS ANALYSIS - abundance, richness over time
+
+add species to nmds with "extreme" values
+species in nmds should help to describe separation or closeness of points/hulls
+
+substrate proportions: different colors, group the groupings by nature vs manmade, remove grouping label on top
+
+
+connecting weekly work - drafting cohesive poster contents
+
+try canonical correspondence analysis?
