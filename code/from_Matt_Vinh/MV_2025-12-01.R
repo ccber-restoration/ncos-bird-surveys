0cf162c38b26e64f030c4d3de18182045ea18c63
Matt Vinh <mattvinh@gmail.com>
2026-01-04 14:44:55 -0800

    modify guild visualizations and todo list

--- code/from_Matt_Vinh/MV_2025-12-01.R
+++ code/from_Matt_Vinh/MV_2025-12-01.R
@@ -4,29 +4,25 @@ breeding period - april to june or july
 
 connecting weekly work - drafting cohesive poster contents
 
+substrate proportions: different colors, group the groupings by nature vs manmade, remove grouping label on top
+
 modify aggregation - include original variables with no recorded values
 
-#TODO
-
+GUILDS ANALYSIS - abundance, richness over time
 
 add vertical lines to visualize season to year + season plots
 
+#TODO
+
+reading papers on stats methods, ecological knowledge relating to work being done
 
 density of watefowl birds or others
 
 google scholar density of birds?
   
-species richness per guild over time
+species richness per guild over time <- ?
 
-reading papers on stats methods, ecological knowledge relating to work being done
-
-indicator species analysis by year
-
-GUILDS ANALYSIS - abundance, richness over time
+indicator species analysis by year <- ?
 
 add species to nmds with "extreme" values
 species in nmds should help to describe separation or closeness of points/hulls
-
-substrate proportions: different colors, group the groupings by nature vs manmade, remove grouping label on top
-
-try canonical correspondence analysis?
