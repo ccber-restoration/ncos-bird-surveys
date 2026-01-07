bba421cdfd8ea2a22c5be6835e3395b497bcae52
Matt Vinh <mattvinh@gmail.com>
2026-01-07 15:18:28 -0800

    add meeting notes

---
+++ code/from_Matt_Vinh/MV_notes_2026-01-07.R
@@ -0,0 +1,20 @@
+# TODO
+
+investigate breeding bird study resources - shiny app, grand santa barbara data collection map
+
+use water level data - compare to what? use to highlight what?
+  
+glm and or glmm accounting for temporal autocorrelation
+
+null model for non replicatable data bootstrapping timeseries
+- null model simulates likelihood of bird abundance vs survey year trend appearing the way that it does when plotted
+
+detrend timeseries / timeseries decomposition
+
+scale_x_date - way to plot time with year and season on x
+
+visualizations like on cornell ebird
+
+focus: trend detection in nonreplicated time series
+
+increasing or decreasing species abundances - individual species!
