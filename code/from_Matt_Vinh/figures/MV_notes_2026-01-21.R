4da6971133621f437192bd3d81a1a8071a344242
Matt Vinh <mattvinh@gmail.com>
2026-01-21 16:17:50 -0800

    add 2026-01-21 meeting notes

---
+++ code/from_Matt_Vinh/figures/MV_notes_2026-01-21.R
@@ -0,0 +1,23 @@
+
+wright analysis of most common birds, written analysis of rare species (burrowing owl, savannah sparrow)
+
+wright data has less water, thus less aviation dynamics
+
+use only one species for popbayes fit_trend model
+
+why couldnt use "category" variable to exclude "sp" and "hybrid" categories for gotelli hierarchichal popbayes model?
+  
+gotelli popbayes formatting by month instead of year for format_data
+
+add to data aggregation:
+  distinct?
+  fill missing values (start time for all observations of a survey)
+  taxonomy? as is in wright paper
+  
+create data frames at the species and survey levels
+- species: just info about each species, total count, number of surveys appearing in
+- survey: no species or counts, but all other variables
+
+lisa:
+  - total number of species over time
+  - presence absence community similarity
