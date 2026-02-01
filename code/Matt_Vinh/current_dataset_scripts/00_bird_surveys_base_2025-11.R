4313a4581e53de5b71c537ea072f7e72812c53e4
Matt Vinh <mattvinh@gmail.com>
2026-02-01 14:16:25 -0800

    modify base dataset cleaning script heavily, restructuring calls, adding notes and making fixes discussed during 2026-01-28 meeting. through line 249

--- code/Matt_Vinh/current_dataset_scripts/00_bird_surveys_base_2025-11.R
+++ code/Matt_Vinh/current_dataset_scripts/00_bird_surveys_base_2025-11.R
@@ -1,126 +1,253 @@
+
+##### libraries ----
+
 library(tidyverse)
 library(here)
 library(janitor)
 
-##### existing aggregated dataset wrangling ####
+
+
+##### existing compiled data cleaning ----
+
 
 birdsurveyfilepath <- here("data","aggregated","dryad_2017-2023",
                            "doi_10_5061_dryad_bvq83bkhz__v20240617",
                            "NCOS_Monthly_Bird_Surveys_2017-2023.csv")
 birdsurveys <- read_csv(birdsurveyfilepath) # 11 variables
 
-# cleaning substrates
-# NA and <Null> substrates become Unknown
-birdsurveys_filtered <- birdsurveys %>%
-  #FIXME make NA counts 1, rather than excluding
-  filter(!is.na(Count)) # removes 6 rows with no recorded species count
-birdsurveys_filtered$Substrate[birdsurveys_filtered$Substrate == "rocks"] <- "Rock"
-birdsurveys_filtered$Substrate[birdsurveys_filtered$Substrate == 
-                                 "Other (Enter in Observation Notes)"] <- "Other"
-birdsurveys_filtered$Substrate[birdsurveys_filtered$Substrate == "<Null>"] <- "Unknown"
-birdsurveys_filtered$Substrate[is.na(birdsurveys_filtered$Substrate)] <- "Unknown"
 
-# cleaning observation date
-# adding survey year and season variables
-birdsurveys_filtered$obs_date2 <- substr(birdsurveys_filtered$Observation.Date,1,10) # + 1 variable
-birdsurveys_filtered <- birdsurveys_filtered %>% # + 3 variables
-  mutate(Observation_Date = parse_date_time(obs_date2,orders = c("mdy","ymd")),
-         Observation_Date = as.Date(Observation_Date),
-         Survey_Year = case_when(
-           Observation_Date >= as.Date("2017-09-01") & Observation_Date <= as.Date("2018-08-31") ~ 1,
-           Observation_Date >= as.Date("2018-09-01") & Observation_Date <= as.Date("2019-08-31") ~ 2,
-           Observation_Date >= as.Date("2019-09-01") & Observation_Date <= as.Date("2020-08-31") ~ 3,
-           Observation_Date >= as.Date("2020-09-01") & Observation_Date <= as.Date("2021-08-31") ~ 4,
-           Observation_Date >= as.Date("2021-09-01") & Observation_Date <= as.Date("2022-08-31") ~ 5,
-           Observation_Date >= as.Date("2022-09-01") & Observation_Date <= as.Date("2023-08-31") ~ 6),
-         Season = case_when(
-           month(Observation_Date) %in% c(1,2,12) ~ "Winter",
-           month(Observation_Date) %in% c(3:5) ~ "Spring",
-           month(Observation_Date) %in% c(6:8) ~ "Summer",
-           month(Observation_Date) %in% c(9:11) ~ "Fall")) %>% 
-  #FIXME- we can probably figure out the correct date for these two rows
-  filter(!is.na(Observation_Date))
-# total 15 variables
+birdsurveys_filtered <- birdsurveys %>% 
+  mutate(
+    
+    ### data type
+    Count = as.integer(Count),
+    
+    ### replacing missing observation counts with 1
+    Count = replace_na(Count,1),
+    
+    ### cleaning substrate names
+    # replacing <Null> and NA substrates with new Unknown level
+    Substrate = replace(Substrate,
+                        Substrate == "rocks",
+                        "Rock"),
+    Substrate = replace(Substrate,
+                        Substrate == "Other (Enter in Observation Notes",
+                        "Other"),
+    Substrate = replace(Substrate,
+                        Substrate == "<Null>",
+                        "Unknown"),
+    Substrate = replace_na(Substrate,"Unknown"),
+    
+    ### cleaning observation date
+    # removing time of observation
+    # - only some observations have time included with observation date
+    # to determine observation date of two rows missing observation date:
+    # - filter birdsurveys by year == 2022 because year of rows missing dates are 2022
+    # - then rows should be entered in chronological order for 2022?
+    # - NA observation dates lie between dates 2022-04-20 and 2022-05-16
+    # - assigning 2022-04-20 to one row and 2022-05-16 to the other
+    obs_date2 = substr(birdsurveys$Observation.Date,1,10),
+    observation_date = parse_date_time(obs_date2,
+                                       orders = c("mdy","ymd")),
+    observation_date = as.Date(observation_date),
+    observation_date = replace(observation_date,
+                               which(is.na(observation_date))[1],
+                               as.Date("2022-04-20")),
+    observation_date = replace(observation_date,
+                               which(is.na(observation_date)),
+                               as.Date("2022-05-16")),
+    
+    ### adding survey year variable
+    survey_year = case_when(
+      observation_date >= as.Date("2017-09-01") & observation_date <= as.Date("2018-08-31") ~ 1,
+      observation_date >= as.Date("2018-09-01") & observation_date <= as.Date("2019-08-31") ~ 2,
+      observation_date >= as.Date("2019-09-01") & observation_date <= as.Date("2020-08-31") ~ 3,
+      observation_date >= as.Date("2020-09-01") & observation_date <= as.Date("2021-08-31") ~ 4,
+      observation_date >= as.Date("2021-09-01") & observation_date <= as.Date("2022-08-31") ~ 5,
+      observation_date >= as.Date("2022-09-01") & observation_date <= as.Date("2023-08-31") ~ 6),
+    
+    ### adding season variable
+    season = case_when(
+      month(observation_date) %in% c(1,2,12) ~ "Winter",
+      month(observation_date) %in% c(3:5) ~ "Spring",
+      month(observation_date) %in% c(6:8) ~ "Summer",
+      month(observation_date) %in% c(9:11) ~ "Fall")) %>% 
+  
+  ### removing intermediate, unnecessary observation date variables
+  select(-c("Observation.Date","obs_date2")) %>% 
+  
+  ### janitor package name cleaning
+  clean_names()
 
 
-##### wrangling recent data (2025-2025) ----
+### declutter
+rm(birdsurveys)
 
-# file paths to folders organized by year containing bird survey data
+
+
+##### wrangling recent data (2023-2025) ----
+
+
 csvpath23 <- here("data","2023")
 csvpath24 <- here("data","2024")
 csvpath25 <- here("data","2025")
 
-# extracting files from file path locations with list.files()
+
+### file extraction with list.files()
 csvfiles23 <- list.files(path = csvpath23,pattern = "2023_(09|10|11|12)_.*\\.csv$",
                          full.names = T)
 csvfiles24 <- list.files(path = csvpath24,pattern = "\\.csv$",full.names = T)
 csvfiles25 <- list.files(path = csvpath25,pattern = "\\.csv$",full.names = T)
 
-# reading files with map_dfr(), creating column for date of bird survey and 
-# editing resulting dates after manual verification of accuracy
+
+### reading files with map_dfr() and cleaning
+# for all survey data objects, observation date variable is added,
+# observation date intermediate variable "parsed" is removed,
+# and NA substrates become new Unknown level
+
 df23 <- map_dfr(csvfiles23,~ read_csv(.x,col_types = cols(.default = "c"),
                                       locale = locale(encoding = "Latin1")) %>% 
                   mutate(
+                    
+                    # intermediate and new variable
                     parsed = mdy_hms(CreationDate),
-                    Observation_Date = as.Date(parsed)))
-
-#FIXME The problem here is that the filename doesn't match the actual survey date
-# (Probably rescheduled due to rain or something)
-#Trust the data, not the filename
-df23$Observation_Date[df23$Observation_Date == "2023-12-06"] <- "2023-12-04"
+                    observation_date = as.Date(parsed),
+                    
+                    # new substrate level
+                    Substrate = replace_na(Substrate,"Unknown"))) %>% 
+  # remove intermediate
+  select(-"parsed")
 
 df24 <- map_dfr(csvfiles24,~ read_csv(.x,col_types = cols(.default = "c"),
                                       locale = locale(encoding = "Latin1")) %>% 
                   mutate(
+                    
+                    # intermediate na dnew variable
                     parsed = mdy_hms(CreationDate),
-                    Observation_Date = as.Date(parsed)))
-df24$Observation_Date[df24$Observation_Date == "2024-08-22"] <- "2024-08-23"
-df24 <- df24 %>% filter(Observation_Date != "2020-10-24")
+                    observation_date = as.Date(parsed),
+                    
+                    # new substrate level
+                    Substrate = replace_na(Substrate,"Unknown"),
+                    
+                    # replacing erroneous date
+                    # 3 observations with "2020-10-24", many more with "2024-10-23"
+                    observation_date = replace(observation_date,
+                                               observation_date == "2020-10-24",
+                                               "2024-10-23"))) %>% 
+  
+  # removing two extra variables with no data, along with intermediate
+  select(-c("parsed",`Direction of travel (°)`,`Compass reading (°)`))
 
 df25 <- map_dfr(csvfiles25,~ read_csv(.x,col_types = cols(.default = "c"),
                                       locale = locale(encoding = "Latin1")) %>% 
                   mutate(
+                    
+                    # intermediate and new variable
                     parsed = mdy_hms(CreationDate),
-                    Observation_Date = as.Date(parsed)))
-df25$Observation_Date[df25$Observation_Date == "2025-04-29"] <- "2025-04-25"
+                    observation_date = as.Date(parsed),
+                    
+                    # new substrate level
+                    Substrate = replace_na(Substrate,"Unknown"))) %>% 
+  
+  # remove intermediate
+  select(-"parsed")
 
-df24 <- df24 %>% select(-c(`Direction of travel (°)`,`Compass reading (°)`))
 
-# converting NA substrate to Unknown
-df23$Substrate[is.na(df23$Substrate)] <- "Unknown"
-df24$Substrate[is.na(df24$Substrate)] <- "Unknown"
-df25$Substrate[is.na(df25$Substrate)] <- "Unknown"
+### rbind-ing bird survey data of each year, new variables and cleaning
+compiled <- rbind(df23,df24,df25) %>% 
+  mutate(
+    
+    ### new variable
+    year = year(observation_date),
+    
+    ### correct data type
+    observation_date = as.Date(observation_date),
+    
+    ### adding survey year variable
+    survey_year = case_when(
+      observation_date >= as.Date("2023-09-01") & observation_date <= as.Date("2024-08-31") ~ 7,
+      observation_date >= as.Date("2024-09-01") & observation_date <= as.Date("2025-08-31") ~ 8),
+    
+    ### adding season variable
+    season = case_when(
+      month(observation_date) %in% c(1,2,12) ~ "Winter",
+      month(observation_date) %in% c(3:5) ~ "Spring",
+      month(observation_date) %in% c(6:8) ~ "Summer",
+      month(observation_date) %in% c(9:11) ~ "Fall"),
+    
+    ### fixing observations with species in notes variable
+    Species = replace(Species,
+                      `Observation Notes` == "American bittern",
+                      "American Bittern"),
+    Species = replace(Species,
+                      `Observation Notes` == "Bittern",
+                      "American Bittern"),
+    Species = replace(Species,
+                      `Observation Notes` == "sharp skinned hawk",
+                      "Sharp-shinned Hawk"),
+    Species = replace(Species,
+                      `Observation Notes` == "hummingbird sp",
+                      "Hummingbird sp."),
+    Species = replace(Species,
+                      `Observation Notes` == "Sharp Shinned hawk",
+                      "Sharp-shinned Hawk"),
+    Species = replace(Species,
+                      `Observation Notes` == "swinhoes white eye",
+                      "Swinhoe's White-eye"),
+    Species = replace(Species,
+                      `Observation Notes` == "Wilson's Phalarope",
+                      "Wilson's Phalarope"),
+    Species = replace(Species,
+                      `Observation Notes` == "Wilson's phalarope",
+                      "Wilson's Phalarope"),
+    Species = replace(Species,
+                      `Observation Notes` == "wilsons phaloropes 4 repeat",
+                      "Wilson's Phalarope"),
+    Species = replace(Species,
+                      `Observation Notes` == "barn owl",
+                      "American Barn Owl"),
+    Species = replace(Species,
+                      `Observation Notes` == "horned lark",
+                      "Horned Lark"),
+    Species = replace(Species,
+                      `Observation Notes` == "mountain bluebird",
+                      "Mountain Bluebird"),
+    Species = replace(Species,
+                      `Observation Notes` == "chestnut backed chickadee",
+                      "Chestnut-backed Chickadee"),
+    Species = replace(Species,
+                      `Observation Notes` == "lazuli bunting",
+                      "Lazuli Bunting"),
+    Species = replace(Species,
+                      `Observation Notes` == "Swinhoes White Eye",
+                      "Swinhoe's White-eye"),
+    Species = replace(Species,
+                      `Observation Notes` == "cost as hummingbird",
+                      "Costa's Hummingbird"),
+    # guessing that this is a peregrine falcon because no other bird species with
+    # "falcon" in the name have been recorded
+    Species = replace(Species,
+                      `Observation Notes` == "falcon ppp flew over",
+                      "Peregrine Falcon"),
+    Species = replace(Species,
+                      `Observation Notes` == "swallow spp",
+                      "Swallow sp."),
+    # one species remains na with the note:
+    # - "orphaned ducklings", row 1449
+    
+    ### filling remaining NA species with new level Unknown
+    Species = replace_na(Species,"Unknown"),
+    
+    ### data type
+    Count = as.integer(Count),
+    
+    ### replacing NA count with 1
+    Count = replace_na(Count,1))
 
-# rbind-ing bird survey data of each year and creating year, survey year and
-# season variables using case_when()
-intermediate <- rbind(df23,df24) # 56 variables
-newaggregate <- rbind(intermediate,df25) %>% mutate( # + 3 variables
-  Year = year(Observation_Date),
-  Observation_Date = as.Date(Observation_Date),
-  Survey_Year = case_when(
-    Observation_Date >= as.Date("2023-09-01") & Observation_Date <= as.Date("2024-08-31") ~ 7,
-    Observation_Date >= as.Date("2024-09-01") & Observation_Date <= as.Date("2025-08-31") ~ 8),
-  Season = case_when(
-    month(Observation_Date) %in% c(1,2,12) ~ "Winter",
-    month(Observation_Date) %in% c(3:5) ~ "Spring",
-    month(Observation_Date) %in% c(6:8) ~ "Summer",
-    month(Observation_Date) %in% c(9:11) ~ "Fall"))
 
-#declutter
-rm(intermediate)
-rm(df23)
-rm(df24)
-rm(df25)
+### declutter
+rm(df23,df24,df25)
 
-# cleaning resulting aggregated data frame
-newaggregate$Count <- as.integer(newaggregate$Count)
-#FIXME- look at these 12 rows...
-newaggregate <- newaggregate %>% filter(!is.na(newaggregate$Count)) # -12 rows
-
-newaggregate <- newaggregate[!is.na(newaggregate$Species),] # -29 rows
-#FIXME- checked these as well. Most of these had OBSERVATION NOTES
-newaggregate_chk <- newaggregate %>% 
-  filter(is.na(Species))
 
 
 ##### compiling aggregates ----
