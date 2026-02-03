45d137760ad5c394b00657031fe54fe142f56525
fjoyce <fhjoyce@ucsb.edu>
2026-02-03 09:57:09 -0800

    get data for network viz

--- code/Morales_Gabriella/trophic_network.R
+++ code/Morales_Gabriella/trophic_network.R
@@ -3,3 +3,20 @@
 
 #load packages
 library(tidyverse)
+library(readxl)
+
+#packages we could use for visualizing networks:
+
+library(foodwebr)
+library(bipartite)
+
+
+links_path <- "data/aquatic_diets/NCOS_aquatic_trophic_links_2026-02-03.xlsx"
+
+trophic_links <- read_xlsx(path = links_path, sheet = "trophic links")
+
+
+#next steps:
+
+#continue filling out trophic links data
+#create draft network viz based on existing data
