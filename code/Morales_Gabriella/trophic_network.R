40615f93c357323f6ebbcda550d472fc6ca68ec9
fjoyce <fhjoyce@ucsb.edu>
2026-02-03 09:31:02 -0800

    add script to visualize trophic network

---
+++ code/Morales_Gabriella/trophic_network.R
@@ -0,0 +1,5 @@
+#this script is for visualizing potential trophic links between aquatic invertebrates and birds at NCOS
+# trophic links were compiled by Gabriella primarily using Birds of the World
+
+#load packages
+library(tidyverse)
