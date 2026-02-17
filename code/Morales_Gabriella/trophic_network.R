81e7f39435a19f25458757b56407afbf3b4ea661
gabriellamorales-tech <gabriellamorales@ucsb.edu>
2026-02-17 09:52:20 -0800

    new code for network

--- code/Morales_Gabriella/trophic_network.R
+++ code/Morales_Gabriella/trophic_network.R
@@ -15,7 +15,7 @@ links_path <- "data/aquatic_diets/NCOS_aquatic_trophic_links_2026-02-10.xlsx"
 #in network terminology this is an "edge list"
 trophic_links <- read_xlsx(path = links_path, sheet = "trophic links") %>% 
   select(c(bird_species, invert_taxon)) %>% 
-  relocate(invert_taxon, .before = bird_species) %>% 
+  #relocate(invert_taxon, .before = bird_species) %>% 
   filter(bird_species != "Whimbrel") %>% 
   filter(bird_species != "Hooded Merganser")
 
@@ -30,8 +30,12 @@ trophic_network <- graph_from_edgelist(tl_matrix, directed = FALSE)
 
 adj_matrix <- as_adjacency_matrix(trophic_network, sparse=FALSE)
 
-matrix_subset <- adj_matrix[inverts, birds]
+matrix_subset <- adj_matrix[birds, inverts]
+
+pdf(file = "code/Matt_Vinh/figures/")
 
 #continue filling out trophic links data
 #create draft network viz based on existing data
-plotweb(web = matrix_subset, text.rot = 90)
+plotweb(web = matrix_subset, text_size =1.1, horizontal = TRUE)
+
+
