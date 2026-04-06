9e20cda9c42afedcc1bdc20eb4bbaf899a31354c
gabriellamorales-tech <gabriellamorales@ucsb.edu>
2026-04-05 18:09:52 -0700

    Added color to foodweb networks.

--- code/Morales_Gabriella/trophic_network.R
+++ code/Morales_Gabriella/trophic_network.R
@@ -35,11 +35,54 @@ adj_matrix <- as_adjacency_matrix(trophic_network, sparse=FALSE)
 
 matrix_subset <- adj_matrix[birds, inverts]
 
+link_colors1 <- matrix("gray80",
+                       nrow = nrow(matrix_subset),
+                       ncol = ncol(matrix_subset))
+
+# Assign colors by prey type
+
+# Ostracoda
+link_colors1[, colnames(matrix_subset) == "Ostracoda"][
+  matrix_subset[, colnames(matrix_subset) == "Ostracoda"] > 0] <- "#264653"
+
+# Corixidae
+link_colors1[, colnames(matrix_subset) == "Corixidae"][
+  matrix_subset[, colnames(matrix_subset) == "Corixidae"] > 0] <- "#287271"
+
+# Chironomidae
+link_colors1[, colnames(matrix_subset) == "Chironomidae"][
+  matrix_subset[, colnames(matrix_subset) == "Chironomidae"] > 0] <- "#2a9d8f"
+
+# Oligochaeta
+link_colors1[, colnames(matrix_subset) == "Oligochaeta"][
+  matrix_subset[, colnames(matrix_subset) == "Oligochaeta"] > 0] <- "#8ab17d"
+
+# Copepoda
+link_colors1[, colnames(matrix_subset) == "Copepoda"][
+  matrix_subset[, colnames(matrix_subset) == "Copepoda"] > 0] <- "#babb74"
+
+# Ephydridae
+link_colors1[, colnames(matrix_subset) == "Ephydridae"][
+  matrix_subset[, colnames(matrix_subset) == "Ephydridae"] > 0] <- "#e9c46a"
+
+# Cladocera
+link_colors1[, colnames(matrix_subset) == "Cladocera"][
+  matrix_subset[, colnames(matrix_subset) == "Cladocera"] > 0] <- "#f4a261"
+
+# Nematoda
+link_colors1[, colnames(matrix_subset) == "Nematoda"][
+  matrix_subset[, colnames(matrix_subset) == "Nematoda"] > 0] <- "#ee8959"
+
+# Ceratopogonidae
+link_colors1[, colnames(matrix_subset) == "Ceratopogonidae"][
+  matrix_subset[, colnames(matrix_subset) == "Ceratopogonidae"] > 0] <- "#e76f51"
+
+
 png(file = "figures/invert_network.png", width = 600, height = 900, units = "px", res = 100)
 
 #continue filling out trophic links data
 #create draft network viz based on existing data
-plotweb(web = matrix_subset, text_size =1.1, horizontal = TRUE)
+plotweb(web = matrix_subset, text_size =1.1, horizontal = TRUE, link_color = link_colors1)
 
 dev.off()
 
@@ -71,12 +114,32 @@ adj_matrix_fish <- as_adjacency_matrix(trophic_network_fish, sparse=FALSE)
 #subset matrix to avoid duplication
 matrix_subset_fish <- adj_matrix_fish[birds_fish_eating, prey]
 
+link_colors <- matrix("gray80",
+                      nrow = nrow(matrix_subset_fish),
+                      ncol = ncol(matrix_subset_fish))
+
+# Assign colors by prey type
+
+# Small fish
+link_colors[, colnames(matrix_subset_fish) == "small fish"][
+  matrix_subset_fish[, colnames(matrix_subset_fish) == "small fish"] > 0] <- "#287271"
+
+# Large fish
+link_colors[, colnames(matrix_subset_fish) == "large fish"][
+  matrix_subset_fish[, colnames(matrix_subset_fish) == "large fish"] > 0] <- "#8ab17d"
+
+# Amphibia
+link_colors[, colnames(matrix_subset_fish) == "Amphibia"][
+  matrix_subset_fish[, colnames(matrix_subset_fish) == "Amphibia"] > 0] <- "#e9c46a"
+
+# Crayfish
+link_colors[, colnames(matrix_subset_fish) == "Procambarus clarkii"][
+  matrix_subset_fish[, colnames(matrix_subset_fish) == "Procambarus clarkii"] > 0] <- "#e76f51"
 
 png(file = "figures/trophic_network_fish_eating.png", width = 600, height = 800, units = "px", res = 100)
 
-#continue filling out trophic links data
-#create draft network viz based on existing data
-plotweb(web = matrix_subset_fish, text_size =1.1, horizontal = TRUE)
+plotweb(web = matrix_subset_fish, text_size =1.1, horizontal = TRUE, link_color = link_colors)
+
 
 dev.off()
 
@@ -88,7 +151,7 @@ dev.off()
 
 png(file = "figures/Food_Webs/trophic_network_fish_eating_phylopic.png", width = 7, height = 7.5, units = "in", res = 200)
 
-plotweb(web = matrix_subset_fish, text_size =1.1, horizontal = TRUE)
+plotweb(web = matrix_subset_fish, text_size =1.1, horizontal = TRUE, link_color = link_colors)
 add_phylopic_base(name = "Mugil cephalus",
                   x = -0.18, y = 0.53,
                   width = 0.2,
@@ -116,7 +179,7 @@ dev.off()
 
 png(file = "figures/Food_Webs/invert_network_phylopic.png", width = 6, height = 8, units = "in", res = 200)
 
-plotweb(web = matrix_subset, text_size =1, horizontal = TRUE, mar = c(2, 0.82, 0.82, 0.42))
+plotweb(web = matrix_subset, text_size =1, horizontal = TRUE, mar = c(2, 0.82, 0.82, 0.42), link_color = link_colors1)
 add_phylopic_base(name = "Cypris", #Ostracoda image 6
                   x = -0.15, y = 0.93,
                   width = 0.11,
